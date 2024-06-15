3725cab97cd346c5feed7b2fdcedf8489aafa332
ehsan <bakefayat98@gmail.com>
2024-06-15 10:58:26 +0330

    gitignore added.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from ads import ads
 
@@ -12,26 +12,28 @@ class Ad(BaseModel):
 
 
 @app.get("/")
-def get_ads():
+def get_ads() -> dict:
     return {"ads": ads}
 
 @app.get("/ad/{ad_id}")
-def get_add(ad_id: int):
-    ad = ads[ad_id]
-    return {"ad": ad}
+def get_add(ad_id: int) -> dict:
+    ad = ads.get(ad_id)
+    if ad:
+        return {"ad": ad}
+    raise HTTPException(status_code=404, detail="No such ad")
 
 @app.post("/ad/create/{ad_id}")
-def create_ad(ad_id: int, ad:Ad):
+def create_ad(ad_id: int, ad:Ad) -> str:
     if ad_id in ads:
-        return {"msg": "ad exists"}
+        raise HTTPException(status_code=400, detail="ad is alredy exists")
     
     ads[ad_id] = ad
     return ads[ad_id]
 
 @app.delete("/ad/delete/{ad_id}")
-def delete_ad(ad_id: int):
+def delete_ad(ad_id: int) -> dict:
     if ad_id not in ads:
-        return {"msg": "not existed"}
+        raise HTTPException(status_code=404, detail="not existed")
     
     ads.pop(ad_id)
     return {"msg": "success"}
