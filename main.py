ac81719bc652eab5e5c294ab7c338d7e2ee15d51
ehsan <bakefayat98@gmail.com>
2024-06-19 14:19:58 +0330

    filtering ads added. schema changed.

--- main.py
+++ main.py
@@ -1,45 +1,43 @@
 from enum import Enum
 from fastapi import FastAPI, HTTPException
-from schemas import AdModel, WorkURLChoices, ads
+from schemas import AdsBaseModel, AdsCreateModel, AdsModel, WorkURLChoices, ads_list
 
 app = FastAPI()
 
 
 @app.get("/ads")
-async def get_ads() -> list[AdModel]:
-    return [
-        AdModel(**a) for a in ads
-    ]
+async def get_ads(
+    work_title: WorkURLChoices = None,
+    gender: str = None,
+    ) -> list[AdsModel]:
+
+    filterd_ads = [AdsModel(**a) for a in ads_list]
+    if work_title:
+        filterd_ads = [a for a in filterd_ads if a.work.value == work_title.value]
+    if gender:
+        filterd_ads = [a for a in filterd_ads if a.gender == gender]
+
+    return filterd_ads
+
+@app.post("/ads", status_code=201)
+async def create_ad(ad:AdsCreateModel) -> AdsModel:
+    id = ads_list[-1]["id"] + 1
+    ad_with_id = AdsModel(id=id, **ad.model_dump()).model_dump()
+    ads_list.append(ad_with_id)
+    return ad_with_id
 
 @app.get("/ads/{ad_id}")
-async def get_add(ad_id: int) -> AdModel:
-    ad = next((ad for ad in ads if ad["id"] == ad_id), None)
+async def get_add(ad_id: int) -> AdsModel:
+    ad = next((ad for ad in ads_list if ad["id"] == ad_id), None)
     if ad is None:
         raise HTTPException(status_code=404, detail="No such ad")
     return ad
 
-@app.get("/ads/work/{work_title}")
-async def get_works(work_title: WorkURLChoices) -> list[AdModel]:
-    ads_based_on_work = [
-        ad for ad in ads if ad["work"] == work_title.value
-    ]
-
-    if not ads_based_on_work:
-        raise HTTPException(status_code=404, detail="No such ad")
-    return ads_based_on_work
-
-@app.post("/ads/create/{ad_id}", status_code=201)
-async def create_ad(ad:AdModel) -> AdModel:
-    existed_ad = next((a for a in ads if a["id"] == ad.id), None)
-    if existed_ad is not None:
-        raise HTTPException(status_code=400, detail="ad is alredy exists")
-    
-    ads.append(dict(ad))
-    return ad
 
 @app.delete("/ads/delete/{ad_id}", status_code=204)
 async def delete_ad(ad_id: int):
-    if ad_id not in ads:
+    ad = next((a for a in ads_list if a["id"] == ad_id), None)
+    if not ad:
         raise HTTPException(status_code=404, detail="not existed")
-    
-    ads.pop(ad_id)
+
+    ads_list.pop(ads_list.index(ad))
--- schemas.py
+++ schemas.py
@@ -6,33 +6,43 @@ class WorkURLChoices(Enum):
     IT = "IT"
 
 
-class AdModel(BaseModel):
-    id: int
+class AdsBaseModel(BaseModel):
     name: str
     work: WorkURLChoices
+    gender: str
     phone: int
     res_phone: int
 
+class AdsCreateModel(AdsBaseModel):
+    pass
 
-ads = [
+
+class AdsModel(AdsBaseModel):
+    id: int
+
+
+ads_list = [
     {
-        "id": 1,
+        "id": 0,
         "name": "ehsan",
         "work": "IT",
+        "gender": "male",
         "phone": 9371304458,
         "res_phone": 9011259601
     },
     {
-        "id": 2,
+        "id": 1,
         "name": "oways",
         "work": "EMS",
+        "gender": "male",
         "phone": 9156962252,
         "res_phone": 9379339007
     },
     {
-        "id": 3,
-        "name": "elyas",
+        "id": 2,
+        "name": "sahar",
         "work": "IT",
+        "gender": "female",
         "phone": 9154879652,
         "res_phone": 9745312548
     }
